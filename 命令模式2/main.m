8e8d615e15a53cc85e00bbf95ce64228e43f5291
梁显杰 <liangxianjie>
2018-06-26 13:32:55 +0800

    Initial Commit

---
+++ 命令模式2/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  命令模式2
+//
+//  Created by zhongding on 2018/6/26.
+//  Copyright © 2018年 zhongding. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
